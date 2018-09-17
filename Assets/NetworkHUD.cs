caeaea5eb85ef265e0a1b9817d1a972ebaa5ca04
MateuszMarkuszewski <markuszewski.mateusz254@gmail.com>
2018-09-17 20:04:21 +0200

    Network Menu

---
+++ Assets/NetworkHUD.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkHUD : MonoBehaviour {
+
+    private NetworkManagerHUD HUD;
+
+    void Start () {
+        HUD = GetComponent<NetworkManagerHUD>();
+	}
+
+    void ShowHUD(bool show)
+    {
+        HUD.showGUI = show;
+    }
+
+    private void OnGUI()
+    {
+        if (HUD.enabled)
+        {
+            HUD.offsetX = Screen.width / 3;
+            HUD.offsetY = Screen.height / 3;
+        }
+    }
+}
